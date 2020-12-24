6382d56590fe05e81c84d9d82a32f981f5b948c3
kazumawada <kazumawadaa@gmail.com>
2020-12-24 11:03:06 +0900

    create header footer #10

---
+++ app/javascript/packs/components/Footer.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>ベルリンなう</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+//   <v-card>
+//     <v-footer
+//       v-bind="localAttrs"
+//       :padless="padless"
+//     >
+//       <v-card
+//         flat
+//         tile
+//         width="100%"
+//         class="red lighten-1 text-center"
+//       >
+//         <v-card-text>
+//           <v-btn
+//             v-for="icon in icons"
+//             :key="icon"
+//             class="mx-4"
+//             icon
+//           >
+//             <v-icon size="24px">
+//               {{ icon }}
+//             </v-icon>
+//           </v-btn>
+//         </v-card-text>
+
+//         <v-divider></v-divider>
+
+//         <v-card-text class="white--text">
+//           {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+//         </v-card-text>
+//       </v-card>
+//     </v-footer>
+
+//   </v-card>
+// </template>
+
+
+//   export default {
+//     data: () => ({
+//       icons: [
+//         'mdi-home',
+//         'mdi-email',
+//         'mdi-calendar',
+//         'mdi-delete',
+//       ],
+//       items: [
+//         'default',
+//         'absolute',
+//         'fixed',
+//       ],
+//       padless: false,
+//       variant: 'default',
+//     }),
+//     computed: {
+//       localAttrs () {
+//         const attrs = {}
+
+//         if (this.variant === 'default') {
+//           attrs.absolute = false
+//           attrs.fixed = false
+//         } else {
+//           attrs[this.variant] = true
+//         }
+//         return attrs
+//       },
+//     },
+//   }
+// </script>
--- app/javascript/packs/components/Header.vue
+++ app/javascript/packs/components/Header.vue
@@ -1,45 +1,54 @@
 <template>
-  <div>
-      <v-app-bar
-        color="gray accent-4"
-        dark
+  <v-card
+    class="mx-auto overflow-hidden"
+    height="400"
+  >
+    <v-app-bar
+      color="deep-purple"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>ベルリンなう</v-toolbar-title>
+      
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
       >
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-  
-        <v-toolbar-title>ベルリンなう</v-toolbar-title>
-  
-        <div class="flex-grow-1"></div>
-  
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-  
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-  
-        <v-menu
-          left
-          bottom
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
         >
-          <template v-slot:activator="{ on }">
-            <v-btn icon v-on="on">
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-          </template>
-  
-          <v-list>
-            <v-list-item
-              v-for="n in 5"
-              :key="n"
-              @click="() => {}"
-            >
-              <v-list-item-title>りんく {{ n }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-app-bar>
-  </div>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
 
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
 </template>
-
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+  }
+</script>
