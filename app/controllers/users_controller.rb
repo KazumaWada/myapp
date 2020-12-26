0599c8f50d4808ea37c5876b08a6bbe95a9184ab
kazumawada <kazumawadaa@gmail.com>
2020-12-26 17:29:33 +0900

    create signup #21

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,35 @@
 class UsersController < ApplicationController
+# コントロールの内容は全部コンソールで実行する時のコマンドなのかな
+
+
   # 特定のユーザーを表示する
   def show
     @user=User.find(params[:id])
     #正常に処理が行われると、@user=User.find(1)となる。
   end
   def new
+    @user = User.new
+  end
+
+
+  # resources :usersをroutes.rbファイルに追加すると（リスト 7.3）自動的にRailsアプリケーションが
+  # 表 7.1のRESTful URI に応答するようになったことを思い出してください。
+  # 特に、/usersへのPOSTリクエストはcreateアクションに送られます。
+  def create
+    @user = User.new(user_params) 
+    if @user.save
+      flash[:success] = "Welcome to ベルリンなう!"
+      # 新規登録成功
+      redirect_to @user
+    else
+      # 新規登録失敗
+      render 'new'
+    end
+  end
+
+   private
+   def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_12_26_025624) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
