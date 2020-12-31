0e6143b3e8b2f3d6c4412289fbcaffafb41e8a2c
kazumawada <kazumawadaa@gmail.com>
2020-12-31 10:21:38 +0900

    create user following #32

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
     has_many :posts, dependent: :destroy
+    ############following関係######################################
+    has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+    has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed#sourceの意味 → following配列の元はfollowedIdのかたまりであるという意味。
+    has_many :followers, through: :passive_relationships, source: :follower
+     ############following関係######################################
     default_scope -> { order(created_at: :desc) }
     attr_accessor :remember_token, :activation_token
     before_save   :downcase_email
@@ -53,6 +63,25 @@ class User < ApplicationRecord
   def feed
     Post.where("user_id = ?", id)#=posts
   end
+
+  #####################followingメソッド###########################
+  #<<は、配列の最後に追記する
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
+
+  ################################################################
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,16 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   #micropostではないので、後からblogPostを編集できるeditが必要。
   resources :posts,          only: [:new, :create, :destroy]
+#_follow_formのrender follow,unfollow用
+  resources :relationships,       only: [:create, :destroy]
 
   # 新規ポストlocalhost:3000/posts/new
+
+
+  #memberで、users/1/followingが実現できる。
+  resources :users do
+    member do 
+      get :following, :followers
+    end
+  end
 end
