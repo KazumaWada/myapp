ad6ededea086d651028f3721a5113b9161b38fa6
kazumawada <kazumawadaa@gmail.com>
2021-03-17 18:22:28 +0900

    ci test #150

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "SignUps", type: :feature do
+  include ActiveJob::TestHelper
+
+   # ユーザーはサインアップに成功する
+   scenario "user successfully signs up" do
+    visit root_path
+    click_link "Signup"
+
+    perform_enqueued_jobs do
+      expect { 
+        fill_in "user_name", with: "testname"
+        fill_in "user_password", with: "test_password"
+        fill_in "user_password_confirmation", with: "test_password"
+        select "1年", from: "user_year"
+        fill_in "user_bio", with: "hello"
+        click_button "アカウント作成"
+      }.to change(User, :count).by(0)
+  end
+end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -20,7 +21,9 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+# RSpecのメソッド/support配下のファイルを読み込む。
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -33,6 +36,9 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+#  sign_up_specのperform_enqueued_jobsをワークさせるため
+  config.include(ActiveSupport::Testing::Assertions)
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -61,4 +67,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  #factory botの設定のため。
+  config.include FactoryBot::Syntax::Methods
+
+  # これのため。gem 'shoulda-matchers'
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails 
+    end 
+  end
+
+#supportファイルの読み込み。helperは、module名がhelperだから。
+config.include Helpers
+
 end
