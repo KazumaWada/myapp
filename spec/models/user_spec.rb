4cccfebf12160288874b61cded0d1c3678de3710
kazumawada <kazumawadaa@gmail.com>
2021-03-19 19:20:20 +0900

    fixed-mysql8-rspec-for-ci #150

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+#本当に正しいテストを書いているのか確認するために、toをto_notに変えてみるというやり方がある。
+RSpec.describe User, type: :model do
+
+  # ここからfactory
+  let(:user) { FactoryBot.create(:user) }
+
+  describe User do 
+    #  有効なfactoryか？
+    it "has a valid factory" do 
+      expect(user).to be_valid
+    end
+  end
+
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_length_of(:name).is_at_most(100) }
+
+  it { is_expected.to validate_presence_of :email }
+  #it { is_expected.to validate_uniqueness_of(:email).case_insensitive } ・・・(*)
+  it { is_expected.to validate_length_of(:email).is_at_most(200) }
+
+  it { is_expected.to validate_presence_of :password }
+  it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  
+  it { is_expected.to validate_presence_of :year }
+  it { is_expected.to validate_length_of(:year).is_at_most(4) }
+
+  it { is_expected.to validate_presence_of :bio }
+  it { is_expected.to validate_length_of(:bio).is_at_most(300) }
+
+  # it "is invalid with a duplicate email adress" do 
+  #   FactoryBot.create(:user, email: "aaron@example.com")
+  #   user = FactoryBot.build(:user, email: "aaron@example.com")
+  #   user.valid?
+  #   expect(user.errors[:email]).to include("has already been taken")
+  # end
+
+#emailの有効性
+describe "email validation with proper address form" do
+   #無効な場合
+   it "is invalid address form" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. 
+      foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        user.email = invalid_address
+        expect(user).to_not be_valid
+      end
+    end
+    #有効な場合
+    it "is valid addressnform" do
+      valid_adresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      valid_adresses.each do |valid_adress|
+        user.email = valid_adress
+        expect(user).to be_valid
+      end
+    end
+end# describe "email validation with proper address form" do
+
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -20,7 +21,14 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
+
+# RSpecのメソッド/support配下のファイルを読み込む。
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join("spec/support/helpers/**/*.rb")].sort.each { |f| require f }
+
+# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -33,6 +41,9 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+#  sign_up_specのperform_enqueued_jobsをワークさせるため
+  config.include(ActiveSupport::Testing::Assertions)
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -56,9 +67,36 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
+  
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  #factory botの設定のため。
+    config.include FactoryBot::Syntax::Methods
+  # これのため。gem 'shoulda-matchers'
+ Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails 
+    end 
+  end
+
+
+
+
+  
+ 
+# config.include UsersHelpers
+#supportファイルの読み込み。helperは、module名がhelperだから。
+#helpersに何も書かれていない&エラーになるから、コメントアウトしておく。
+#  config.include UsersHelpers
+#詳しいエラー
+# An error occurred while loading ./spec/features/login_spec.rb.
+# Failure/Error: config.include helpers
+
+
+# NameError:
+#   undefined local variable or method `helpers' for main:Object
+
 end
